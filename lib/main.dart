695c4fed2efaefd99f0c883d9d0640fbf4772851
John Paul Ong <jpong5202@gmail.com>
2023-04-14 08:49:32 +0800

    Add initial light theme data

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,28 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  ThemeData lightThemeData = ThemeData(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color.fromARGB(255, 0, 100, 255),
+      onPrimary: Colors.white,
+      secondary: Color.fromARGB(255, 255, 100, 100),
+      onSecondary: Colors.white,
+      error: Color.fromARGB(255, 255, 0, 0),
+      onError: Colors.white,
+      background: Colors.white,
+      onBackground: Colors.black,
+      surface: Colors.grey,
+      onSurface: Colors.black,
+    ),
+  );
+
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: lightThemeData,
     ),
   );
 }
